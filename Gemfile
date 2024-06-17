715bf37bd0a7ebb61a270279811cfbe26473db9e
Aleks Rūtiņš <aleks@rutins.com>
2024-06-17 18:20:40 -0400

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+# gem "rails"
+
+gem "sinatra", "~> 4.0"
+gem "redis-rb", "~> 9001.0"
+
+gem "haml", "~> 6.3"
+
+gem "rackup", "~> 2.1"
+
+gem "rerun", "~> 0.14.0"
+
+gem "kramdown", "~> 2.4"
+
+gem "redis", "~> 5.2"
+
+gem 'dotenv', groups: [:development, :test]
---
+++ app.rb
@@ -0,0 +1,43 @@
+require 'dotenv/load'
+require 'sinatra'
+require 'kramdown'
+require 'redis'
+
+helpers do
+  def format_date(date)
+    Date.parse(date).to_s
+  end
+end
+
+redis = Redis.new(url: ENV['REDIS_URL'])
+
+set :haml, {escape_html: false}
+
+get '/' do
+  haml :index, locals: {
+    title: 'Home',
+    show_home: false,
+    author_name: redis.get('author_name'),
+    posts: redis.keys('post:*').map {|key| JSON.parse(redis.get(key)).merge(id: key.split(':').last) }
+  }
+end
+
+get '/p/new' do
+  haml :publish, locals: {title: 'Publish', show_home: true}
+end
+
+post '/p/new' do
+  return false unless params[:token] == ENV['PUBLISH_TOKEN']
+
+  redis.set("post:#{params[:slug]}",
+            params.slice(:title, :published_on, :summary, :body)
+            .merge(
+              published_on: Date.today.to_s,
+            ).to_json)
+  redirect '/'
+end
+
+get '/p/:post' do
+  post = JSON.parse(redis.get("post:#{params[:post]}"))
+  haml :post, locals: post.merge(show_home: true)
+end
